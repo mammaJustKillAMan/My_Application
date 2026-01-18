db66f6c9c47b5dddb35231867b12fb92b715da0a
mammaJustKillAMan <nata.jalosinska@gmail.com>
2026-01-18 19:03:35 +0100

    deleted

--- app/src/main/java/com/example/myapplication/navigation/MainScreen.kt
+++ app/src/main/java/com/example/myapplication/navigation/MainScreen.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication.navigation
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
