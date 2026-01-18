232be48e000250dc5471e20968f926fb3a23d6f8
mammaJustKillAMan <nata.jalosinska@gmail.com>
2026-01-18 19:18:24 +0100

    added

--- app/src/main/java/com/example/myapplication/navigation/MainScreen.kt
+++ app/src/main/java/com/example/myapplication/navigation/MainScreen.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
