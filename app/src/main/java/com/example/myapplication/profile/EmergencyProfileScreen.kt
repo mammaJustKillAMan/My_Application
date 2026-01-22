b9ecbb1eaaaf7b49262cce7ad1761e536abd6766
mammaJustKillAMan <nata.jalosinska@gmail.com>
2026-01-22 21:55:34 +0100

    updated

--- app/src/main/java/com/example/myapplication/profile/EmergencyProfileScreen.kt
+++ app/src/main/java/com/example/myapplication/profile/EmergencyProfileScreen.kt
@@ -6,7 +6,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.auth.AuthViewModel
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.auth.AuthState
+import com.example.myapplication.auth.AuthState
 
 @Composable
 fun EmergencyProfileScreen(
