9266e76acd456b90f5d99600fb3307c3b11c323e
mammaJustKillAMan <nata.jalosinska@gmail.com>
2026-01-22 21:55:21 +0100

    updated

--- app/src/main/java/com/example/myapplication/altitude/AltitudeState.kt
+++ app/src/main/java/com/example/myapplication/altitude/AltitudeState.kt
@@ -1,9 +1,9 @@
 package com.example.myapplication.altitude
 
 data class AltitudeState(
-    val currentAltitude: Double,
-    val previousAltitude: Double,
-    val ascentRate: Double,
-    val riskLevel: RiskLevel,
-    val isLoading: Boolean
+    val currentAltitude: Double = 0.0,
+    val previousAltitude: Double = 0.0,
+    val ascentRate: Double = 0.0,
+    val riskLevel: RiskLevel = RiskLevel.LOW,
+    val isLoading: Boolean = false
 )
