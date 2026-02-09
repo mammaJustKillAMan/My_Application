6e87f7f2fde0b38a675017791d80c870448caa70
mammaJustKillAMan <nata.jalosinska@gmail.com>
2026-02-09 23:26:14 +0100

    imports added

--- app/src/main/java/com/example/myapplication/profile/EmergencyProfileScreen.kt
+++ app/src/main/java/com/example/myapplication/profile/EmergencyProfileScreen.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.profile
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
