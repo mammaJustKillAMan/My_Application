12407a3a0a70af3c9f5dc8e2a2342672207f0878
mammaJustKillAMan <nata.jalosinska@gmail.com>
2026-02-09 23:42:27 +0100

    nth

--- app/src/main/java/com/example/myapplication/medical/EmergencyCallScreen.kt
+++ app/src/main/java/com/example/myapplication/medical/EmergencyCallScreen.kt
@@ -3,7 +3,6 @@ package com.example.myapplication.medical
 import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
