829c09f4374b8ccab5642c98c91a1fbb27b9fc66
mammaJustKillAMan <nata.jalosinska@gmail.com>
2026-02-09 23:51:25 +0100

    imports added

--- app/src/main/java/com/example/myapplication/auth/AuthNavGraph.kt
+++ app/src/main/java/com/example/myapplication/auth/AuthNavGraph.kt
@@ -1,8 +1,7 @@
 package com.example.myapplication.auth
 
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 
