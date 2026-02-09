a39afc5f3726d365e4c2247797eb023db3846494
mammaJustKillAMan <nata.jalosinska@gmail.com>
2026-02-09 23:26:38 +0100

    imports added

--- app/src/main/java/com/example/myapplication/profile/EmergencyProfileScreen.kt
+++ app/src/main/java/com/example/myapplication/profile/EmergencyProfileScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.auth.AuthViewModel
